303065957f18802c5f75b480866b427ec09900df
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2024-01-03 11:49:38 +0100

    project Initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <Button>NexaPulse</Button>
+    </main>
+  );
+}
+
+export default App;
