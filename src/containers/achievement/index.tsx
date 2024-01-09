140c96e3f676bbc5573fb2136af09f4c91b2ad6a
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2024-01-09 05:27:39 +0100

    Addition of Achievement and Portfolio

--- src/containers/achievement/index.tsx
+++ src/containers/achievement/index.tsx
@@ -1,7 +1,28 @@
+import projects from "@/assets/images/projects.svg";
+import clients from "@/assets/images/clients.svg";
+import experience from "@/assets/images/experience.svg";
 export default function Achievement() {
   return (
-    <section className="container" id="achievement">
-      Achievement
+    <section className="w-full bg-secondary" id="achievement">
+      <div className="container py-14 flex flex-col lg:flex-row gap-10 lg:gap-20 items-center justify-center">
+        {achievements.map((a, index) => (
+          <div key={index} className="flex items-center gap-2">
+            <div className="w-[150px] h-[150px] rounded-full bg-primary flex justify-center items-center">
+              <img src={a.icon} alt="icon" className="w-[75px] h-[75px]" />
+            </div>
+            <div className="flex flex-col items-center text-white">
+              <h6 className="font-heading text-5xl">{a.value}</h6>
+              <p className="text-lg text-center">{a.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
+
+const achievements = [
+  { title: "Projects Completed", value: "200+", icon: projects },
+  { title: "Clients Worldwide", value: "50+", icon: clients },
+  { title: "Experience year", value: "10+", icon: experience },
+];
--- src/containers/portfolio/index.tsx
+++ src/containers/portfolio/index.tsx
@@ -1,7 +1,34 @@
+import img1 from "@/assets/images/portfolio 1.png";
+import img2 from "@/assets/images/portfolio 2.png";
+import img3 from "@/assets/images/portfolio 3.png";
+import img4 from "@/assets/images/portfolio 4.png";
+import img5 from "@/assets/images/portfolio 5.png";
+import img6 from "@/assets/images/portfolio 6.png";
 export default function Portfolio() {
   return (
-    <section className="container" id="portfolio">
-      portfolio
+    <section
+      className="container py-14 flex flex-col items-center gap-10 scroll-m-20"
+      id="portfolio"
+    >
+      <h2 className="text-5xl text-primary font-heading">Our Portfolio</h2>
+      <div className="flex flex-col sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-3 gap-20">
+        {portfolio.map((p, index) => (
+          <div key={index} className="flex flex-col gap-4">
+            <img src={p.img} alt="portfolio img" />
+            <div className="rounded-xl bg-primary p-3 text-center text-white">
+              {p.text}
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
+const portfolio = [
+  { img: img1, text: "A food website company" },
+  { img: img2, text: "A loan finance company" },
+  { img: img3, text: "A Family health planning company" },
+  { img: img4, text: "An e-commerce fashion company" },
+  { img: img5, text: "A venture capital company" },
+  { img: img6, text: "A food website company" },
+];
